195892b3ecb59c55411b77e263392f6a7e5f99dc
Kirill Shklyaev <kirillshklyaev05@gmail.com>
2025-06-02 15:01:41 +0500

    Website is ready

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,12 @@
   <div class="flex flex-col min-h-screen">
     <header class="w-full bg-gradient-to-r from-gray-900 via-gray-950 to-gray-900 text-white shadow-md py-4 px-6 relative z-50">
   <div class="flex items-center justify-between">
-    <!-- Logo -->
+    
     <div class="flex items-center space-x-4">
       <img src="assets/images/arizonalogo.png" class="w-14 h-14 rounded-full border-4 border-white shadow-lg" alt="Logo" />
     </div>
 
-    <!-- Desktop Nav -->
+    
     <nav class="hidden sm:flex items-center gap-6 text-sm font-medium">
       <NuxtLink to="/" class="hover:text-gray-300 transition">Home</NuxtLink>
 
@@ -24,16 +24,8 @@
       <NuxtLink to="/login" class="hover:text-gray-300 transition">LogIn</NuxtLink>
       <NuxtLink to="/logout" class="hover:text-gray-300 transition">LogOut</NuxtLink>
     </nav>
-
-    <!-- Burger -->
-    <button @click="switch_burger" class="sm:hidden flex flex-col justify-center items-center space-y-1">
-      <span :class="['w-6 h-[2px] bg-white transition', burger ? 'rotate-45 translate-y-[7px]' : '']"></span>
-      <span :class="['w-6 h-[2px] bg-white transition', burger ? 'opacity-0' : '']"></span>
-      <span :class="['w-6 h-[2px] bg-white transition', burger ? '-rotate-45 -translate-y-[7px]' : '']"></span>
-    </button>
   </div>
 
-  <!-- Mobile Menu -->
    <transition name="fade">
     <div v-if="burger" class="flex flex-col gap-3 mt-4 sm:hidden bg-gray-900 p-4 rounded-md z-40">
       <NuxtLink to="/" @click="switch_burger" class="text-white text-center">Home</NuxtLink>
