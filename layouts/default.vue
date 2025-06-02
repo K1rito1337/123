df10798e988d21951c8c8d439d1469c15f3d6c51
Kirill Shklyaev <kirillshklyaev05@gmail.com>
2025-06-02 13:54:07 +0500

    Website is ready

---
+++ layouts/default.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header class="flex flex-row w-full bg-gradient-to-r from-gray-900 via-gray-950 to-gray-900 text-white shadow-md py-4 px-6 relative">
+      <div class="basis-1/4 flex items-center justify-start px-4">
+        <img src="assets/images/arizonalogo.png" class="w-20 h-20 rounded-full border-4 border-white shadow-lg" alt="Logo" />
+      </div>
+      <div class="basis-1/4"></div>
+      <nav
+  :class="{
+    'basis-1/2 flex flex-row items-center justify-end px-8 gap-4': !burger || burger,
+    'max-sm:basis-auto max-sm:absolute max-sm:top-full max-sm:w-full max-sm:justify-center max-sm:flex-col max-sm:gap-2 max-sm:bg-gray-900 max-sm:px-4 max-sm:py-4 max-sm:text-white': burger,
+    'max-sm:hidden': !burger,}"
+>
+  <NuxtLink to="/" @click="burger = false" class="px-4 py-2 rounded hover:bg-gray-700 transition max-sm:w-full max-sm:text-center">Home</NuxtLink>
+
+  <div @click="switch_submenu" class="px-4 py-2 rounded hover:bg-gray-700 cursor-pointer transition max-sm:w-full max-sm:text-center">Labs</div>
+
+  <div class="flex flex-col absolute top-full bg-gray-900 text-white text-sm shadow-lg rounded mt-1 z-40 max-sm:relative max-sm:w-full max-sm:bg-gray-800" v-show="submenu">
+    <NuxtLink to="/lab3" class="px-4 py-2 hover:bg-gray-700 transition max-sm:text-center">Lab3</NuxtLink>
+    <NuxtLink to="/lab4" class="px-4 py-2 hover:bg-gray-700 transition max-sm:text-center">Lab4</NuxtLink>
+    <NuxtLink to="/lab5" class="px-4 py-2 hover:bg-gray-700 transition max-sm:text-center">Lab5</NuxtLink>
+    <NuxtLink to="/lab6" class="px-4 py-2 hover:bg-gray-700 transition max-sm:text-center">Lab6</NuxtLink>
+  </div>
+
+  <NuxtLink to="/login" class="px-4 py-2 rounded hover:bg-gray-700 transition max-sm:w-full max-sm:text-center">
+    LogIn
+  </NuxtLink>
+  <NuxtLink to="/logout" class="px-4 py-2 rounded hover:bg-gray-700 transition max-sm:w-full max-sm:text-center">
+    LogOut
+  </NuxtLink>
+</nav>
+
+
+      
+      <div
+        v-if="!burger"
+        @click="switch_burger"
+        class="z-50 cursor-pointer hidden max-sm:flex flex-col mr-4 justify-between items-center w-8 h-6"
+      >
+        <span class="h-[3px] w-full bg-white"></span>
+        <span class="h-[3px] w-full bg-white"></span>
+        <span class="h-[3px] w-full bg-white"></span>
+      </div>
+      <div
+        v-else
+        @click="switch_burger"
+        class="z-50 cursor-pointer hidden max-sm:flex flex-col mr-4 justify-between items-center w-8 h-6"
+      >
+        <span class="h-[3px] w-full bg-white rotate-45 relative top-[9px]"></span>
+        <span class="h-[3px] w-full bg-white opacity-0"></span>
+        <span class="h-[3px] w-full bg-white -rotate-45 relative bottom-[9px]"></span>
+      </div>
+    </header>
+
+    
+    <main class="flex-grow flex flex-col items-center justify-center bg-gray-100 py-12 px-4">
+      <slot />
+    </main>
+
+   
+    <footer class="w-full bg-gray-800 text-white py-6">
+      <div class="flex justify-center gap-8">
+        <a href="https://www.youtube.com/" target="_blank" rel="noopener">
+          <img src="assets/images/youtube.png" class="w-12 h-12 hover:scale-110 transition-transform" alt="YouTube" />
+        </a>
+        <a href="https://github.com/" target="_blank" rel="noopener">
+          <img src="assets/images/github.png" class="w-12 h-12 hover:scale-110 transition-transform" alt="GitHub" />
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener">
+          <img src="assets/images/facebook.png" class="w-12 h-12 hover:scale-110 transition-transform" alt="Facebook" />
+        </a>
+      </div>
+      <p class="mt-4 text-sm text-gray-400">© 2025 Your Company. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useHead } from '#app'
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-9SKVQGS9YV',
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-9SKVQGS9YV');
+      `,
+      type: 'text/javascript',
+    },
+  ],
+  __dangerouslyDisableSanitizersByTagID: {
+    gtag: ['innerHTML'],
+  },
+})
+
+const burger = ref(false)
+const submenu = ref(false)
+
+const switch_burger = () => {
+  burger.value = !burger.value
+  if (!burger.value) submenu.value = false
+}
+
+const switch_submenu = () => {
+  submenu.value = !submenu.value
+}
+</script>
