3b2f033050d0911c2a52d90e325e5e5125cf760c
Kirill Shklyaev <kirillshklyaev05@gmail.com>
2025-06-03 13:53:13 +0500

    Website is ready

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="flex flex-row w-full bg-gradient-to-r from-gray-900 via-gray-950 to-gray-900 text-white shadow-md py-4 px-6 relative">
       <div class="basis-1/4 flex items-center justify-start px-4">
         <img src="assets/images/arizonalogo.png"
-     class="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-white shadow-lg"
+     class="w-16 h-16 bg-white rounded-full p-1 shadow-md hover:scale-110 transition-transform"
      alt="Logo" />
       </div>
       <div class="basis-1/4"></div>
